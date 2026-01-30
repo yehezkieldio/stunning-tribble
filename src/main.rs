e2b24b0b69caa5a6741fce5181312a5889f80c57
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2026-01-30 10:21:47 +0800

    feat(main): simplify main function by removing elaborate_hello

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
 fn main() {
-    let name = "Aristella";
-    let times = 3;
-    elaborate_hello(name, times);
-}
-
-fn elaborate_hello(name: &str, times: usize) {
-    for i in 1..=times {
-        println!("{}. Hello, {}! Welcome to the world of Rust.", i, name);
-    }
+    println!("Hello, world!");
 }
