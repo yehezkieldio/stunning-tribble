2550f9b14bea9729ef6e09ebf171b738078c0ffe
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2026-01-30 09:26:53 +0800

    feat: stuff

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
-    let greeting = "Hello, world!";
-    println!("{}", greeting);
+    let name = "Alice";
+    let times = 3;
+    elaborate_hello(name, times);
+}
+
+fn elaborate_hello(name: &str, times: usize) {
+    for i in 1..=times {
+        println!("{}. Hello, {}! Welcome to the world of Rust.", i, name);
+    }
+    println!("Hope you have a wonderful day, {}!", name);
 }
